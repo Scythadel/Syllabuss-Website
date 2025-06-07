5fd93e8b4d1dd596ac804d48b32d0948d55633c9
Scythadel <pradhamp@hotmail.com>
2025-06-07 19:06:42 +0300

    Centered logo and added search bar

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def test():
-    return 'Hello'
-
-if __name__ == "__main__":
-    app.run(host = '0.0.0.0', debug = True)
+    return render_template('home.html')
